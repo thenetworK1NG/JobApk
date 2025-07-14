2cfdeaf55eebbac09157de8711d68f864d714b5e
thenetworK1NG <thenetworking888@gmail.com>
2025-07-14 10:55:22 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const cacheName = 'mypwa-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => {
+      return res || fetch(event.request);
+    })
+  );
+});
