a1c1b0d36fbe9fa344ca3fb371734f5f0240bfd3
thenetworK1NG <thenetworking888@gmail.com>
2025-07-29 13:53:21 +0200

    Add files via upload

---
+++ start-windows.bat
@@ -0,0 +1,15 @@
+@echo off
+echo Starting Job System PWA Server...
+echo.
+echo The app will be available at: http://localhost:3000
+echo.
+echo Desktop Installation Instructions:
+echo 1. Open Chrome, Edge, or Firefox
+echo 2. Go to http://localhost:3000
+echo 3. Click "Install on Windows" button
+echo 4. The app will be installed as a desktop application
+echo.
+echo Press Ctrl+C to stop the server
+echo.
+node server.js
+pause
